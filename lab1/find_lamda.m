6b7f0cf0339930ebfdada20d67c102cce8fa3d66
1190201115 <57440962+1190201115@users.noreply.github.com>
2021-10-08 10:54:31 +0800

    Rename find_lamda.m to lab1/find_lamda.m

--- lab1/find_lamda.m
+++ lab1/find_lamda.m
@@ -1,12 +1,12 @@
-%%Ñ°ÕÒºÏÊÊµÄ³Í·£ÏµÊı
+%%å¯»æ‰¾åˆé€‚çš„æƒ©ç½šç³»æ•°
 function [ ] = find_lamda(X,data_x,data_y,num)
 siny=sin(2*pi*data_x)';
 
-%%lamda·¶Î§Îªexp(-40),µ½exp(0)
+%%lamdaèŒƒå›´ä¸ºexp(-40),åˆ°exp(0)
 lnlamda=(-40:0.1:0);
 lamda=exp(lnlamda);
 
-%%´æ´¢´ú¼ÛÖµ
+%%å­˜å‚¨ä»£ä»·å€¼
 rms=zeros(401,1);
 rms2=zeros(401,1);
 
@@ -18,7 +18,7 @@ minlamda=0;
 for i=1:401
     w=pinv(X'*X+lamda(i)*eye(col))*X'*data_y;
     rms2(i)=((cal_loss(order,w,data_x,siny))^(1/2))/num;
-    %%¼ÇÂ¼´ú¼Û×îĞ¡Ê±lamdaµÄÖµ£¬×ª»¯ÎªÒÔeÎªµ×µÄÖ¸Êı
+    %%è®°å½•ä»£ä»·æœ€å°æ—¶lamdaçš„å€¼ï¼Œè½¬åŒ–ä¸ºä»¥eä¸ºåº•çš„æŒ‡æ•°
     if rms2(i)<minloss
         minlamda=round(lnlamda(i));
         minloss=rms2(i);
